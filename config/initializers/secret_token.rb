b8c1e316a60518eb737ab59eccdca750556d270a
pox hemma <perca944@gmail.com>
2011-07-02 11:50:31 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ridemo::Application.config.secret_token = '44edff8d65afdc5c0852d7447b52ed1c818c856134f9597f2e3befca17b4db0993a75052a5d705971614dfcd112f291db753cb337cebf129b3c538109e1f3e25'
